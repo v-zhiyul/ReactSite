98e631b353ac5f82541aceb1dbaa5770f2cd99f3
Mitch Webster <Mitchell.Webster@microsoft.com>
2020-02-20 10:15:39 -0800

    try function

--- app/src/index.js
+++ app/src/index.js
@@ -3,9 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 //import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+class FetchDemo extends React.Component {
+    state = {
+      posts: []
+    }
+  
+    componentDidMount() {
+      axios.get(`/.api/${this.props.subreddit}`)
+        .then(res => {
+          console.log(res.data)
+        });
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>trying something</h1>
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <FetchDemo subreddit="getList"/>,
+    document.getElementById('root')
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
