9a87bd3cedcc2fc07c05bf4b53cb42b0165a301e
miwebst <50640090+miwebst@users.noreply.github.com>
2020-04-10 16:06:06 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -2,6 +2,6 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
-        body: JSON.Parse(req.headers['x-ms-client-principal'])
+        body: req.headers
     }
 };
