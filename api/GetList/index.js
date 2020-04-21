292568422f2882288e29a727f196302eb37e8f83
miwebst <50640090+miwebst@users.noreply.github.com>
2020-04-21 09:40:23 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -1,7 +1,24 @@
+function getUserInfo(req){
+    const clientPrincipalHeader = 'x-ms-client-principal';
+    if (req.headers[clientPrincipalHeader] == null) {
+        return null;
+    }
+
+    const buffer = Buffer.from(req.headers[clientPrincipalHeader], 'base64');
+    const serializedJson = buffer.toString('ascii');
+    return JSON.parse(serializedJson);
+}
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    
+    const user = getUserInfo(req);
+    if (user === null)
+    {
+        user = {}
+    }
 
     context.res = {
-        body: req.headers['x-ms-client-principal']
+        body: user
     }
 };
